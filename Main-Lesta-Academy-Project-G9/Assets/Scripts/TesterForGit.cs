d00136336b2c73c7f13c019c4746ccfb41ef3936
Vladislav Shalkovskiy <vlad48516@mail.com>
2022-12-04 12:20:19 +0300

    Made new Script to test without conflict

---
+++ Main-Lesta-Academy-Project-G9/Assets/Scripts/TesterForGit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesterForGit : MonoBehaviour
+{
+    void Start()
+    {
+       //Some changes
+       //Some more changes without conflict 
+    }
+
+    void Update()
+    {
+        
+    }
+}
