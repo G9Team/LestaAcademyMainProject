9894318cc4be7e0637e75311ee809db445a9d1e0
Vladislav Shalkovskiy <vlad48516@gmail.com>
2022-12-13 21:59:27 +0300

    Make vsrataya battle system

---
+++ Main-Lesta-Academy-Project-G9/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] private float _runSpeed;
+    private GameObject _player = null;
+    private int _health = 2;
+    private int _atackDamage = 1;
+    private Rigidbody _rigidBody;
+
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        
+        if (Vector3.Distance(this.transform.position, _player.transform.position) < 10f)
+        {
+            FollowPlayer(_player.transform.position);
+            
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "PlayerAtackHitbox") {
+        int damage = other.transform.parent.GetComponent<PlayerController>().AtackForce;
+        GetDamage(damage);
+        }
+        
+
+    }
+   
+    private void OnCollisionEnter(Collision other)
+    {
+        //Debug.Log(other.collider);
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        //Debug.Log(player);
+        if (player is null) return;
+        player.GetDamage(_atackDamage);
+    }
+    private void FollowPlayer(Vector3 playerPosition)
+    {
+        //Debug.Log("Following Player");
+        float direction = _runSpeed * Mathf.Sign(playerPosition.x - transform.position.x);
+        //Debug.Log(Mathf.Abs(playerPosition.x - transform.position.x));
+        _rigidBody.velocity = new Vector3(direction, _rigidBody.velocity.y, 0);
+        //transform.Translate((playerPosition - transform.position) * _runSpeed);
+    }
+    private void GetDamage(int inputDamage)
+    {
+        if (_health - inputDamage <= 0) Die();
+        _health -= inputDamage;
+        Debug.Log("Auch, it hurts!");
+    }
+    private void Die()
+    {
+        Debug.Log("OMG, u killed Teddy bear!!! He is freaking DEAD now! ");
+        Destroy(this.gameObject);
+    }
+}
--- Main-Lesta-Academy-Project-G9/Assets/Scripts/PlayerController.cs
+++ Main-Lesta-Academy-Project-G9/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float runSpeed;
     [SerializeField] private float _jumpforce;
-    [SerializeField] private Transform _legsPoint;
+    [SerializeField] private GameObject _hitboxPrefab;
+    [SerializeField] private Transform _legsPosition;
     Rigidbody playerRigidbody;
     Animator playerAnimator;
     private int _jumpcounter, _maxJumps = 1;
-    bool isFacingRight;
-
+    bool isFacingRight, _isAtacking = false, _isInvincible = false;
+    private int _health = 4;
+    public int AtackForce {get; private set; } = 1;
     void Start()
     {
         _jumpcounter = _maxJumps;
@@ -21,13 +23,10 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        playerRigidbody.AddForce(Physics.gravity * 5 * playerRigidbody.mass);
-        if (Physics.CheckSphere(transform.position, 0.1f)) _jumpcounter = _maxJumps;
-        if (Input.GetKey(KeyCode.Space) && _jumpcounter > 0) {
-        playerRigidbody.AddForce(Vector3.up * _jumpforce, ForceMode.Impulse);
-        
-        _jumpcounter--;
+        if((Input.GetKey(KeyCode.E) || Input.GetMouseButton(0)) && !_isAtacking){
+            Atack();
         }
+        CheckForJumpingConditions();
         float move = Input.GetAxis("Horizontal");
         playerAnimator.SetFloat("Speed", Mathf.Abs(move));
         playerRigidbody.velocity = new Vector3(move * runSpeed, playerRigidbody.velocity.y, 0);
@@ -35,12 +34,50 @@ public class PlayerController : MonoBehaviour
         else if(move<0 && !isFacingRight) Flip();
         
     }
-
+    private void CheckForJumpingConditions(){
+        playerRigidbody.AddForce(Physics.gravity * 5 * playerRigidbody.mass);
+        if (Physics.CheckSphere(_legsPosition.position, 0.05f)) _jumpcounter = _maxJumps;
+        if (Input.GetKey(KeyCode.Space) && _jumpcounter > 0) {
+        playerRigidbody.AddForce(Vector3.up * _jumpforce, ForceMode.Impulse);
+        _jumpcounter--;
+        }
+    }
     void Flip() 
     {
         isFacingRight = !isFacingRight;
         Vector3 localScale = transform.localScale;
         localScale.z = -localScale.z;
         transform.localScale = localScale;
+    }
+    public void GetDamage(int acceptedDamage) => StartCoroutine(ApplyDamage(acceptedDamage));
+    private IEnumerator ApplyDamage(int acceptedDamage){
+        if (_isInvincible) yield return null;
+        if(_health - acceptedDamage <= 0) Die();
+        Debug.Log("hit! ");
+        _isInvincible = true;
+        _health -= acceptedDamage;
+        Debug.Log("Damage accepted " + acceptedDamage + " Health left: " + _health);
+        yield return new WaitForSeconds (0.6f);
+        _isInvincible = false;
+
+    }
+    private void Die(){
+        Debug.Log("U are dead ");
+        Destroy(this.gameObject);
+    }
+    private void Atack(){
+        StartCoroutine(HitBoxApplier());
+    }
+    private IEnumerator HitBoxApplier(){
+        _isAtacking = true;
+        GameObject hitbox = Instantiate(_hitboxPrefab, 
+            new Vector3(
+                this.transform.position.x + (isFacingRight ? -0.5f : 0.5f), 
+                this.transform.position.y + 1.5f, 
+                this.transform.position.z),
+            Quaternion.identity, this.transform);
+        Destroy(hitbox, 0.2f);
+        yield return new WaitForSeconds(0.4f);
+        _isAtacking = false;
     }
 }
