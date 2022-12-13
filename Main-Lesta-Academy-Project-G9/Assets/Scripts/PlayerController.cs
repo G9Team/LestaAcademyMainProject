2a5ce13db0d6dc667b094c0754501c80e08f3780
Vladislav Shalkovskiy <vlad48516@gmail.com>
2022-12-13 17:55:33 +0300

    make shit sample jumping scene, run and enjoy, puppies

--- Main-Lesta-Academy-Project-G9/Assets/Scripts/PlayerController.cs
+++ Main-Lesta-Academy-Project-G9/Assets/Scripts/PlayerController.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float runSpeed;
-
+    [SerializeField] private float _jumpforce;
+    [SerializeField] private Transform _legsPoint;
     Rigidbody playerRigidbody;
     Animator playerAnimator;
-
+    private int _jumpcounter, _maxJumps = 1;
     bool isFacingRight;
 
     void Start()
     {
+        _jumpcounter = _maxJumps;
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
+        playerRigidbody.AddForce(Physics.gravity * 5 * playerRigidbody.mass);
+        if (Physics.CheckSphere(transform.position, 0.1f)) _jumpcounter = _maxJumps;
+        if (Input.GetKey(KeyCode.Space) && _jumpcounter > 0) {
+        playerRigidbody.AddForce(Vector3.up * _jumpforce, ForceMode.Impulse);
+        
+        _jumpcounter--;
+        }
         float move = Input.GetAxis("Horizontal");
         playerAnimator.SetFloat("Speed", Mathf.Abs(move));
         playerRigidbody.velocity = new Vector3(move * runSpeed, playerRigidbody.velocity.y, 0);
