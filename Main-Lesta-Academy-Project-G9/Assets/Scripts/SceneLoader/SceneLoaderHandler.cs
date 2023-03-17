400c080ce2df9dc1b90d0da7c0e48e59947f328a
Aizyka <seng_1111@mail.ru>
2023-03-17 19:57:52 +0300

    added scene transition

---
+++ Main-Lesta-Academy-Project-G9/Assets/Scripts/SceneLoader/SceneLoaderHandler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoaderHandler : MonoBehaviour
+{
+
+}
+
+public class SceneLoader
+{
+    public static void LoadScene(string sceneName)
+    {
+        LoadScene(SceneManager.GetSceneByName(sceneName).buildIndex);
+    }
+    public static void LoadScene(int sceneIndex)
+    {
+        GameObject newGameObject = new GameObject("LoadHandler");
+        GameObject.DontDestroyOnLoad(newGameObject);
+        newGameObject.AddComponent<SceneLoaderHandler>().StartCoroutine(MainCoroutine(newGameObject,sceneIndex));
+    }
+
+    static IEnumerator MainCoroutine(GameObject handler,int sceneIndex)
+    {
+        GameObject sceneCanvas = GameObject.Instantiate(Resources.Load("SceneLoaderCanvas") as GameObject);
+        GameObject.DontDestroyOnLoad(sceneCanvas);
+        Animator animator = sceneCanvas.GetComponent<Animator>();
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
+            yield return new WaitForSeconds(Time.deltaTime);
+        animator.StopPlayback();
+        Text loadingText = sceneCanvas.transform.Find("LoadWindow/LoadingText").GetComponent<Text>();
+        Text hintText = sceneCanvas.transform.Find("LoadWindow/Hint").GetComponent<Text>(); //idk about hints for now
+        Image fadeImage = sceneCanvas.transform.Find("LoadWindow/Fade").GetComponent<Image>();
+
+        sceneCanvas.transform.Find("LoadWindow").gameObject.SetActive(true);
+        Color col = Color.black;
+        while(col.a > 0)
+        {
+            col.a -= Time.deltaTime * 1.5f;
+            fadeImage.color = col;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        AsyncOperation sceneLoadAsync = SceneManager.LoadSceneAsync(sceneIndex);
+        while(!sceneLoadAsync.isDone)
+        {
+            loadingText.text = $"Loading {(int)(sceneLoadAsync.progress*100f)}%";
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        loadingText.text = $"Loading 100%";
+        while (col.a < 1f)
+        {
+            col.a += Time.deltaTime * 2f;
+            fadeImage.color = col;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        sceneCanvas.transform.Find("LoadWindow").gameObject.SetActive(false);
+        animator.SetTrigger("Next");
+        float normalizedTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime + 1f;
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < normalizedTime)
+            yield return new WaitForSeconds(0.1f);
+        GameObject.Destroy(sceneCanvas);
+        GameObject.Destroy(handler);
+    }
+}
