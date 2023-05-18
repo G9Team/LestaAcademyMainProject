9aad4ff2092e7a2a659bb7ff1bdf5f25d77356e9
Vladislav Shalkovskiy <vlad48516@gmail.com>
2023-05-19 00:55:04 +0300

    Add UI to collectables, add elevator logic in level 1.1

--- Main-Lesta-Academy-Project-G9/Assets/Scripts/New/Detectors/PlayerInteractionsChecker.cs
+++ Main-Lesta-Academy-Project-G9/Assets/Scripts/New/Detectors/PlayerInteractionsChecker.cs
@@ -11,6 +11,7 @@ namespace New
     {
         [SerializeField] private LayerMask _interactableOnly;
         [SerializeField] private float _checkRdius;
+        [SerializeField] private GameObject _hint;
         public bool IsInteractionAvaliable()
         {
             if (Physics.CheckSphere(this.transform.position, _checkRdius, _interactableOnly)){
@@ -24,6 +25,14 @@ namespace New
             Collider[] interactableWithinRadius = Physics.OverlapSphere(this.transform.position, _checkRdius, _interactableOnly);
             return interactableWithinRadius[0].GetComponent<IInteractable>();
 
+        }
+
+        private void Update() {
+            if (Physics.CheckSphere(this.transform.position, _checkRdius, _interactableOnly)){
+                _hint.SetActive(true);
+            }else{
+                _hint.SetActive(false);
+            }
         }
     }
 
