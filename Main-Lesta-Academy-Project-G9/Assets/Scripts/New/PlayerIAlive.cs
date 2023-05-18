8bee672863b6e2e907cb47c05ee3559803ffc952
IJWMG <105589920+IJWMG@users.noreply.github.com>
2023-05-18 09:45:54 +0300

    Create PlayerIAlive.cs

---
+++ Main-Lesta-Academy-Project-G9/Assets/Scripts/New/PlayerIAlive.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace New{
+   
+    public class PlayerIAlive : MonoBehaviour, IAlive
+    {
+        private IPlayerData _playerData;
+        private PlayerComponentManager _manager;
+        private int _health = 0;
+        private void Awake(){
+            _manager = GetComponent<PlayerComponentManager>();
+            _playerData = _manager.GetPlayerData();
+            _health = _playerData.CurrentHealth;
+        }
+
+        public int GetHealth() => _health;
+        public void SetHealth(int health) {
+            _playerData.SetHealth(health);
+            RefreshHealth();
+        }
+        public void ChangeHealth(int health) {
+            _manager.DamagePlayer(health);
+            RefreshHealth();
+        }
+        
+        private void RefreshHealth() => _health = _playerData.CurrentHealth;
+
+    }
+
+}
