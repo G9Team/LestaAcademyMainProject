d571e0d9791bde5674f6e961777fd6a70c38e468
Aizyka <seng_1111@mail.ru>
2023-05-20 19:17:09 +0300

    dialog update

--- Main-Lesta-Academy-Project-G9/Assets/Scripts/Dialogs/DialogMain.cs
+++ Main-Lesta-Academy-Project-G9/Assets/Scripts/Dialogs/DialogMain.cs
@@ -9,7 +9,8 @@ public class DialogMain : MonoBehaviour
     float _updateTime = 0.05f;
     float _dialogTime;
     Text _messageText;
-    private Text _answersText;
+    Text _answersText;
+    AudioSource _source;
     Coroutine _currentUpdateTextCoroutine;
     private Color _noActiveColor;
     private New.InputManager _inputManager;
@@ -17,6 +18,7 @@ public class DialogMain : MonoBehaviour
 
     private void Start()
     {
+        _source = GetComponent<AudioSource>();
         _messageText = transform.Find("Parent/MessagePanel/MSG").GetComponent<Text>();
         _answersText = transform.Find("Parent/MessagePanel/MSG_Answers").GetComponent<Text>();
         _inputManager = FindObjectOfType<New.InputManager>();
@@ -40,7 +42,7 @@ public class DialogMain : MonoBehaviour
 
     IEnumerator TextUpdate()
     {
-        while(_messageText.text != _currentDialog.arrayGroups[_currentDialogPosition].dialog)
+        while (_messageText.text != _currentDialog.arrayGroups[_currentDialogPosition].dialog)
         {
             yield return new WaitForSeconds(_updateTime);
             _messageText.text = _currentDialog.arrayGroups[_currentDialogPosition].dialog.Substring(0, _messageText.text.Length + 1);
@@ -50,23 +52,26 @@ public class DialogMain : MonoBehaviour
     void SelectNext()
     {
         Transform answersParent = transform.Find("Parent/MessagePanel/Answers");
-        for(int i = 1; i < answersParent.childCount; i++)
+        for (int i = 1; i < answersParent.childCount; i++)
         {
             Destroy(answersParent.GetChild(i).gameObject);
         }
         _dialogTime = 0f;
         _currentDialogPosition += 1;
+        _source.Stop();
         if (_currentDialogPosition >= _currentDialog.arrayGroups.Length)
         {
             Stop();
             return;
         }
-        switch(_currentDialog.arrayGroups[_currentDialogPosition].groupType)
+        switch (_currentDialog.arrayGroups[_currentDialogPosition].groupType)
         {
             case DialogAsset.ArrayGroup.GroupType.DIALOG:
                 _messageText.text = "";
                 _messageText.enabled = true;
                 _answersText.enabled = false;
+                _source.clip = _currentDialog.arrayGroups[_currentDialogPosition].clip;
+                _source.Play();
                 foreach (DialogAsset.ArrayCharacters ac in _currentDialog.arrayCharacters)
                 {
                     if (ac.characterName == _currentDialog.arrayGroups[_currentDialogPosition].characterName)
@@ -104,7 +109,7 @@ public class DialogMain : MonoBehaviour
                 _answersText.text = _messageText.text;
                 _answersText.enabled = true;
                 GameObject panel = transform.Find("Parent/MessagePanel/Answers/Panel").gameObject;
-                for(int i = 0; i < _currentDialog.arrayGroups[_currentDialogPosition].answers.Length; i++)
+                for (int i = 0; i < _currentDialog.arrayGroups[_currentDialogPosition].answers.Length; i++)
                 {
                     GameObject inst = Instantiate(panel, answersParent);
                     inst.GetComponentInChildren<Text>().text = _currentDialog.arrayGroups[_currentDialogPosition].answers[i].msg;
@@ -134,9 +139,9 @@ public class DialogMain : MonoBehaviour
 
     void RunEvent(string eventName)
     {
-        foreach(DialogEvent de in FindObjectsOfType<DialogEvent>())
+        foreach (DialogEvent de in FindObjectsOfType<DialogEvent>())
         {
-            if(de.eventName == eventName)
+            if (de.eventName == eventName)
             {
                 de.Run();
             }
@@ -164,7 +169,7 @@ public class DialogMain : MonoBehaviour
     {
         if (!_currentDialog) return;
         _dialogTime += Time.deltaTime;
-        if(_currentDialog.arrayGroups[_currentDialogPosition].groupType == DialogAsset.ArrayGroup.GroupType.DIALOG)
+        if (_currentDialog.arrayGroups[_currentDialogPosition].groupType == DialogAsset.ArrayGroup.GroupType.DIALOG)
         {
             if (Input.GetMouseButtonDown(0) && _messageText.text != _currentDialog.arrayGroups[_currentDialogPosition].dialog)
             {
@@ -176,12 +181,12 @@ public class DialogMain : MonoBehaviour
                 if (_dialogTime > _currentDialog.arrayGroups[_currentDialogPosition].minDialogTime)
                 {
                     if (_currentDialog.arrayGroups[_currentDialogPosition].force > 0)
-                        _currentDialogPosition = _currentDialog.arrayGroups[_currentDialogPosition].force-1;
+                        _currentDialogPosition = _currentDialog.arrayGroups[_currentDialogPosition].force - 1;
                     SelectNext();
                 }
             }
         }
-        else if(_currentDialog.arrayGroups[_currentDialogPosition].groupType == DialogAsset.ArrayGroup.GroupType.EVENT)
+        else if (_currentDialog.arrayGroups[_currentDialogPosition].groupType == DialogAsset.ArrayGroup.GroupType.EVENT)
         {
             if (_dialogTime > _currentDialog.arrayGroups[_currentDialogPosition].minDialogTime)
             {
